7253d3446786c3e7502b63c61ff5358363f2d79a
brianuuu <38166666+brianuuu@users.noreply.github.com>
2026-01-27 00:48:30 +0000

    Changed object link lines to light-cyan

--- src/SonicGLvl/ObjectLinkNode.cpp
+++ src/SonicGLvl/ObjectLinkNode.cpp
@@ -9,6 +9,7 @@ ObjectLinkNode::ObjectLinkNode(Ogre::SceneManager* scene_manager, LibGens::Objec
 	scene_node->getUserObjectBindings().setUserAny("EditorNodePtr", Ogre::Any((EditorNode*)this));
 
 	lines = new DynamicLines(Ogre::RenderOperation::OT_LINE_LIST);
+	lines->setMaterial("objectlink");
 	scene_node->attachObject(lines);
 
 	parent = parent_p;
